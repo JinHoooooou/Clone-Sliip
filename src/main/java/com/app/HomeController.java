cfbce3ae75e7e02aa1aabe1e4d57c8158c6038c0
JinHoooooou <jinho4744@naver.com>
2021-06-05 18:26:44 +0900

    add HomeController

--- src/main/java/com/app/HomeController.java
+++ src/main/java/com/app/HomeController.java
@@ -1,5 +1,13 @@
 package com.app;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
+  @GetMapping("home")
+  public String home() {
+    return "index";
+  }
 }
