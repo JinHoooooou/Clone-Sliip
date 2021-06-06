1e6c3733e46a1243c319c8873c18ad0f47305a69
JinHoooooou <jinho4744@naver.com>
2021-06-06 17:37:06 +0900

    회원 가입 페이지 추가

--- src/main/java/com/app/HomeController.java
+++ src/main/java/com/app/HomeController.java
@@ -2,6 +2,7 @@ package com.app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,10 @@ public class HomeController {
   @GetMapping("")
   public String home() {
     return "index";
+  }
+
+  @GetMapping("/signUpForm")
+  public String signUpForm() {
+    return "signUpForm";
   }
 }
