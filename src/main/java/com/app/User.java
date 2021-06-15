5f3f6169e08b6627e033409e01d71d2d7d535c76
JinHoooooou <jinho4744@naver.com>
2021-06-15 14:36:05 +0900

    User VO 추가

---
+++ src/main/java/com/app/User.java
@@ -0,0 +1,24 @@
+package com.app;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @Column(nullable = false, length = 15)
+  private String userId;
+  @Column(nullable = false, length = 10)
+  private String password;
+  @Column(nullable = false)
+  private String name;
+  private String email;
+}
