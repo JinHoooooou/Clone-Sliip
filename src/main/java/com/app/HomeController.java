a68edc106df740926cdd1dc5dfc847182f0575ef
JinHoooooou <jinho4744@naver.com>
2021-06-15 14:37:40 +0900

    UserService를 통해 데이터 처리

--- src/main/java/com/app/HomeController.java
+++ src/main/java/com/app/HomeController.java
@@ -1,11 +1,15 @@
 package com.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
+
+  @Autowired
+  private UserService userService;
 
   @GetMapping("")
   public String home() {
