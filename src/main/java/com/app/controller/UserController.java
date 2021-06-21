ae065bab997bc2cee32d2a8a891f1dfd8bfe6abb
JinHoooooou <jinho4744@naver.com>
2021-06-21 15:53:42 +0900

    dto, controller, service, repository 패키지 추가

--- src/main/java/com/app/controller/UserController.java
+++ src/main/java/com/app/controller/UserController.java
@@ -1,8 +1,9 @@
-package com.app;
+package com.app.controller;
 
+import com.app.dto.User;
+import com.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
--- src/main/java/com/app/repository/UserRepository.java
+++ src/main/java/com/app/repository/UserRepository.java
@@ -1,5 +1,6 @@
-package com.app;
+package com.app.repository;
 
+import com.app.dto.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
