a005d591cc01c39db636e933e3ee3c5ef081720e
JinHoooooou <jinho4744@naver.com>
2021-06-21 15:55:21 +0900

    상위 url users 추가

--- src/main/java/com/app/controller/UserController.java
+++ src/main/java/com/app/controller/UserController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
   @Autowired
@@ -18,7 +20,7 @@ public class UserController {
     return "signUpForm";
   }
 
-  @PostMapping("/create")
+  @PostMapping("")
   public String create(User user) {
     userService.create(user);
     return "redirect:";
